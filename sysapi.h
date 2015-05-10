53999489c5bbed038d0fe1011c973ffce90c3275
Devendra <devendra.aaru@gmail.com>
2015-05-10 16:23:59 -0400

    add a common headerfile

---
+++ sysapi.h
@@ -0,0 +1,19 @@
+#ifndef __SYSAPI_H__
+#define __SYSAPI_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "sysapi_net.h"
+#include "sysapi_event.h"
+#include "sysapi_time.h"
+
+#endif
+
