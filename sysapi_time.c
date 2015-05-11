77cfe61319bd54b0fcbd57915628591c7bdddcca
Devendra <devendra.aaru@gmail.com>
2015-05-11 00:17:16 -0400

    two more API for time functions

--- sysapi_time.c
+++ sysapi_time.c
@@ -29,6 +29,57 @@ int sapi_is_dst_active()
     return t->tm_isdst > 0? 1: 0;
 }
 
+// the first argument here is for thread safety
+int sapi_get_cur_caltime(struct sapi_time *sapi_time)
+{
+    int ret = -1
+    time_t now;
+    struct tm *t;
+
+    now = time(0);
+    t = localtime(&now);
+    if (t) {
+        ret = 0;
+        sapi_time->sec = t->tm_sec;
+        sapi_time->min = t->tm_min;
+        sapi_time->hour = t->tm_hour;
+        sapi_time->day = t->tm_mday;
+        sapi_time->mon = t->tm_mon;
+        sapi_time->year = t->tm_year + 1900; // get current year
+    }
+
+    return ret;
+}
+
+int sapi_get_cur_monstr(char *mon)
+{
+    int ret = -1;
+    char *months[] = {
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    };
+#define MON_LEN (sizeof(months) / sizeof(months[0]))
+    struct sapi_time cur;
+
+    ret = sapi_get_cur_caltime(&cur);
+    if (ret < 0)
+        return ret;
+
+    ret = 0;
+    strcpy(mon, months[cur.mon]);
+    return ret;
+}
+
 #ifdef CONFIG_SYSAPI_TIME_TEST
 int main(void)
 {
--- sysapi_time.h
+++ sysapi_time.h
@@ -7,5 +7,18 @@ uint64_t sapi_get_time_msec();
 
 int sapi_is_dst_active();
 
+struct sapi_time {
+	int sec;
+	int min;
+	int hour;
+	int day;
+	int mon;
+	int year;
+};
+
+int sapi_get_cur_caltime(struct sapi_time *);
+
+int sapi_get_cur_monstr(char *);
+
 #endif
 
