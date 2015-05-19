7e04b12d612f5b0306ae95d9d1b3665284e7dcb6
DevNaga <devendra.aaru@outlook.com>
2015-05-19 13:22:44 -0400

    add sapi_hashtbl.h header

---
+++ sysapi_hashtbl.h
@@ -0,0 +1,29 @@
+#ifndef __SYSAPI_HASHTBL_H__
+#define __SYSAPI_HASHTBL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+void *sapi_hashtbl_init(int tbl_len);
+
+void sapi_hashtbl_deinit(void *handle,
+                             void (*freefunc)(void *app_ctx, void *data),
+                             void *app_cb);
+                             
+unsigned int sapi_hash(void *ctx, char *name);
+
+void *sapi_get_elem(void *ctx, char *name);
+
+void sapi_delete_hlist(void *handle,
+                             void (*freefunc)(void *app_ctx, void *data),
+                             void *app_cb);
+                             
+void sapi_elem_for_each(void *handle,
+                             void (*usrcb)(void *app_ctx, void *data),
+                             void *app_cb);
+                             
+int sapi_add_helem(void *ctx, void *data, char *name);
+
+#endif
