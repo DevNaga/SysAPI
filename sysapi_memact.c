0e6065f992f1fe036bcac8a6d4b2c6c9cd4617d6
DevNaga <devendra.aaru@outlook.com>
2015-05-21 12:54:44 -0400

    add memory controller in userspace
    this is a simple memory controller that limits a program's memory usage to not consume lots of memory and use as and when its needed.

---
+++ sysapi_memact.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sysapi_memact_data {
+    int memory_limit;
+    int memory_len;
+};
+
+void *sapi_memact_init(int memory_limit)
+{
+    struct sysapi_memact_data *memact;
+
+    memact = calloc(1, sizeof(struct sysapi_memact_data));
+    if (!memact)
+        return core;
+
+    memact->memory_limit = memory_limit;
+
+    return core;
+}
+
+void *sapi_memact_alloc(void *cb, int size)
+{
+    struct sysapi_memact_data *memact = cb;
+
+    if (memact->memory_len + size >= memact->memory_limit)
+        return NULL;
+
+    memact->memory_len += size;
+
+    return calloc(1, size);
+}
+
+void sapi_memact_expand(void *cb, int exp_len)
+{
+    struct sysapi_memact_data *memact = cb;
+
+    cb->memory_limit += exp_len;
+}
+
+void sapi_memact_deinit(void *cb)
+{
+    free(cb);
+}
