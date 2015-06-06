4376515edcc07064b04ab9fb24da16255cd106b4
Devendra <devendra.aaru@gmail.com>
2015-06-06 05:15:29 -0400

    add comments to the API

--- sysapi_memact.h
+++ sysapi_memact.h
@@ -4,9 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// initialise memory accounting module
+// memory_limit is the total limit on the
+// program's memory usage.
 void *sapi_memact_init(int memory_limit);
+
+// use this instead of calling c(m)alloc
+// allocation fails, if the memory requested
+// will be more than the limit specified at
+// the initialisation procedure
 void *sapi_memact_alloc(void *cb, int size);
+
+// expand the memory limit on the total program
+// allocation
+// only call if needed.. 
 void sapi_memact_expand(void *cb, int exp_len);
+
+// deinit the memory accounting module
 void sapi_memact_deinit(void *cb);
 
 #endif
