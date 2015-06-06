5f064ac196db34c973e4c54b3e77afa7c6f10de3
Devendra <devendra.aaru@gmail.com>
2015-06-06 02:22:52 -0400

    test program

---
+++ tests/test_syscirque.c
@@ -0,0 +1,45 @@
+#include "../sysapi.h"
+
+void cb_caller(void *cbdata, void *data)
+{
+    int *i;
+
+    i = data;
+
+    printf("%d ", *i);
+}
+
+int main(void)
+{
+    int i;
+    int array[10];
+    void *handler;
+
+    handler = sysapi_cirque_init(10);
+
+    for (i = 0; i < 10; i++) {
+        array[i] = i;
+        sysapi_cirque_add(handler, &(array[i]));
+    }
+
+    sysapi_cirque_for_each(handler, NULL, cb_caller);
+
+    sysapi_cirque_deinit(handler);
+
+    printf("\n");
+
+    handler = sysapi_cirque_init(6);
+
+    for (i = 0; i < 10; i++) {
+        array[i] = i;
+        sysapi_cirque_add(handler, &(array[i]));
+    }
+
+    sysapi_cirque_for_each(handler, NULL, cb_caller);
+
+    sysapi_cirque_deinit(handler);
+
+    printf("\n");
+    return 0;
+}
+
