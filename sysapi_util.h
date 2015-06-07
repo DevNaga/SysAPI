71d4b35e7e883b8349f000ad093ced8d35593611
Devendra <devendra.aaru@gmail.com>
2015-06-07 04:57:55 -0400

    API documentation

--- sysapi_util.h
+++ sysapi_util.h
@@ -8,9 +8,20 @@
 #define SYSAPI_LEVEL_WARN 2
 #define SYSAPI_LEVEL_ERR  1
 
+// log the informational message
+// this will show up green on the console
 #define sysapi_log_info(msg, ...) sysapi_printf(SYSAPI_LEVEL_INFO, msg " "__VA_ARGS__)
+
+// log the warning message
+// this will show up yellow on the console
 #define sysapi_log_warn(msg, ...) sysapi_printf(SYSAPI_LEVEL_WARN, msg " "__VA_ARGS__)
+
+// log the error message
+// this will show up red on the console
 #define sysapi_log_err(msg, ...) sysapi_printf(SYSAPI_LEVEL_ERR, msg " " __VA_ARGS__)
+
+// log the normal informational message
+// this will show up as a normal message on the console
 #define sysapi_log_norm(msg, ...) sysapi_printf(SYSAPI_LEVEL_NORM, msg " " __VA_ARGS__)
 
 #endif
