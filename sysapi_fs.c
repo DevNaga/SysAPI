e850edd4611b2d4486ccf5d1588bec528e3bdfbe
devnaga <devendra.aaru@outlook.com>
2015-06-25 03:57:43 -0400

    fix if the directory tree is only a set of directories
    The callback will be called if the type is a file or a directory now. The type is added to the callback to say the content of the "file" portion.

--- sysapi_fs.c
+++ sysapi_fs.c
@@ -113,7 +113,7 @@ int sysapi_touch(char *filename)
 }
 
 int sysapi_dir_walk(char *dirpath,
-                    void (*callback)(char *parent, char *filename, void *app_ctx),
+                    void (*callback)(char *parent, sysapi_file_type file_type, char *filename, void *app_ctx),
                     void *app_ctx)
 {
     DIR *dirp;
@@ -128,7 +128,10 @@ int sysapi_dir_walk(char *dirpath,
     while ((entry = readdir(dirp)) != NULL) {
         memset(path, 0, sizeof(path));
         strcpy(path, dirpath);
-        strcat(path, "/");
+
+        if (path[strlen(path) - 1] != '/')
+            strcat(path, "/");
+
         strncat(path, entry->d_name, strlen(entry->d_name));
 
         struct stat sf;
@@ -136,12 +139,14 @@ int sysapi_dir_walk(char *dirpath,
         stat(path, &sf);
 
         if (S_ISREG(sf.st_mode)) {
-            callback(dirpath, path, app_ctx);
+            callback(dirpath, SYSAPI_FILE_TYPE_REGFILE, path, app_ctx);
         } else if (S_ISDIR(sf.st_mode)) {
-            strcat(path, "/");
+            if (path[strlen(path) - 1] != '/')
+                strcat(path, "/");
             if (!strcmp(entry->d_name, ".") ||
                 !strcmp(entry->d_name, "..")) {
             } else {
+                callback(dirpath, SYSAPI_FILE_TYPE_DIRECT, path, app_ctx);
                 sysapi_dir_walk(path, callback, app_ctx);
             }
         }
@@ -287,5 +292,3 @@ int sapi_ramfs_read(void *fs_data, void *data, int len)
 
     return len;
 }
-
-
