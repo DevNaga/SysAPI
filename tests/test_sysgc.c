415b02120fd0567dc9c3fbfc0f98d0ad78ec3e5f
devnaga <devendra.aaru@outlook.com>
2015-06-30 00:33:08 -0400

    Gc tests
    valgrind -v --leak-check=full ./tests/test_sysgc gives me 0 leaks

---
+++ tests/test_sysgc.c
@@ -0,0 +1,34 @@
+#include "../sysapi_gc.h"
+
+void *reference;
+
+void scoped_allocator(int loops, int size)
+{
+    void *var;
+
+    var = SAPI_SCOPED_MALLOC(reference, size);
+    var = SAPI_SCOPED_CALLOC(reference, size);
+
+    sapi_gc_cleanup_scope(reference);
+}
+void allocator(int loops, int size)
+{
+    int i;
+    void *mem;
+
+    for (i = 0; i < loops; i++) {
+        mem = SAPI_MALLOC(reference, size);
+        printf("allocated %p %d\n", mem, loops);
+    }
+    loops--;
+    if (loops <= 0)
+        return;
+    //allocator(loops, 20);
+}
+int main(void) {
+    reference = sapi_gc_startup();
+    scoped_allocator(200, 20);
+    allocator(200, 20);
+    sapi_gc_cleanup(reference);
+    return 0;
+}
