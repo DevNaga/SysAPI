8b500260722ec3b57698658c3f543fbf531cd78d
devnaga <devendra.aaru@outlook.com>
2015-07-07 03:28:43 -0400

    fixes to the util test program

--- tests/test_sysutil.c
+++ tests/test_sysutil.c
@@ -2,13 +2,15 @@
 
 int main(void) {
     int a = 1, b = 2;
+
     sysapi_log_info("test", "This is an info msg\n");
     sysapi_log_warn("test: ", "This is a warning msg\n");
     sysapi_log_err("test: ", "This is an err msg\n");
     sysapi_log_info("testing", "%d %d\n", a, b);
     sysapi_log_norm("testing", "%d %d\n", a, b);
-    printf("%d\n", sysapi_get_arch());
+    printf("%s\n", (sysapi_get_arch() == SYSAPI_ARCH_64_BIT)? "64": "32");
     printf("%d\n", sysapi_get_rand());
     printf("%d\n", sysapi_get_rand_range(22));
-}
 
+    return 0;
+}
