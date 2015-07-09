d6351c0f115c1f7102aa33f829ae786334b9e848
devnaga <devendra.aaru@outlook.com>
2015-07-09 03:29:36 -0400

    include list.h

--- sysapi.h
+++ sysapi.h
@@ -20,6 +20,7 @@
 #include "sysapi_memact.h"
 #include "sysapi_cirque.h"
 #include "sysapi_util.h"
+#include "sysapi_list.h"
 #include "sysapi_dllist.h"
 #include "sysapi_thpool.h"
 #include "sysapi_gc.h"
